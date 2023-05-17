ea098ab09d45005d4437e09a9b2ce0b1f63f32ba
Freddy-ux <alfredmuleri569@gmail.com>
2023-05-17 21:37:06 +0300

    Done With Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
-class Dog
+class Dog 
 
-end
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
+
+        def name
+            @this_dogs_name 
+        end
+    end
+
+    lassie = Dog.new
+    lassie.name = "lassie"
+
+    puts lassie
+
+
